60a92e4d16e9d0946e743a0fa457b03006d9abfe
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/WebSite/StandalonePage.aspx.vb
@@ -0,0 +1,9 @@
+Imports Microsoft.VisualBasic
+Imports System
+
+Partial Public Class StandalonePage
+	Inherits System.Web.UI.Page
+	Protected Sub Page_PreInit(ByVal sender As Object, ByVal e As EventArgs)
+		DevExpress.Web.ASPxClasses.ASPxWebControl.SetIECompatibilityMode(7, Me)
+	End Sub
+End Class
